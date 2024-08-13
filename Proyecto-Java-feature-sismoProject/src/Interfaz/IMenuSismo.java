d1a84bb408432475648a6a0cf355b76822d96a81
Miguel Montúfar <157767334+23BigMike@users.noreply.github.com>
2024-08-13 00:33:09 -0500

    Delete Proyecto-Java-feature-sismoProject directory

--- Proyecto-Java-feature-sismoProject/src/Interfaz/IMenuSismo.java
+++
@@ -1,9 +0,0 @@
-package Interfaz;
-
-import Clases.Sismo;
-
-public interface IMenuSismo {
-    public void registrarSismo(String pCedula, String pNombre, String pTipo, String pMagnitud, String pProfundidad, String pComuna);
-    public Sismo buscarSismo(String codigo, String fecha, String magnitud );
-
-}
